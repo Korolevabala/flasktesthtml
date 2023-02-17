f3f8a841b9f178da368fbabe5906554c1e9c4c4b
Korolevabala <pamitdura228@gmail.com>
2023-02-17 12:56:38 +0300

    add html

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ['О сайте', 'О авторе', 'Пример страницы']
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html', title='Flask first')
+    return render_template('index.html', title='Flask first', menu = menu)
 
 
 if __name__ == '__main__':
